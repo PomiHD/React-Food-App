42cdc422a24cd60da248edffde2611ce5ab58046
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-03-19 22:08:52 +1000

    add meals component and styling.

--- src/components/AvailableMeals.tsx
+++ src/components/AvailableMeals.tsx
@@ -1,6 +1,7 @@
 ﻿import { useEffect, useState } from "react";
 import { fetchAvailableMeals } from "../htpp.ts";
 import { Error } from "./Error.tsx";
+import Meals from "./Meals.tsx";
 
 export default function AvailableMeals() {
   const [isFecthing, setIsFetching] = useState(false);
@@ -32,24 +33,12 @@ export default function AvailableMeals() {
   }
   return (
     <>
-      {!isFecthing && availableMeals.length > 0 && (
-        <div>
-          <ul id={"meals"}>
-            {availableMeals.map((meal) => (
-              <li key={meal.id} className={"meal-item"}>
-                <img
-                  src={`http://localhost:3000/${meal.image}`}
-                  alt={meal.image}
-                />
-                <h3> {meal.name}</h3>
-                <p> ${meal.price}</p>
-                <p id="meal-item-description">{meal.description}</p>
-                <button className={"button"}>Add</button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <Meals
+        meals={availableMeals}
+        isLoading={isFecthing}
+        loadingText="Loading meals data..."
+        fallbackText="No meals available."
+      />
     </>
   );
 }
---
+++ src/components/Meals.tsx
@@ -0,0 +1,29 @@
+﻿export default function Meals({ meals, isLoading, loadingText, fallbackText }) {
+  return (
+    <>
+      {" "}
+      {isLoading && (
+        <div className={"mx-auto text-center border-b-orange-700"}>
+          <p className={"text-red-600"}>{loadingText}</p>
+        </div>
+      )}
+      {!isLoading && meals.length === 0 && <p>{fallbackText}</p>}
+      {!isLoading && meals.length > 0 && (
+        <ul id={"meals"}>
+          {meals.map((meal) => (
+            <li key={meal.id} className={"meal-item"}>
+              <img
+                src={`http://localhost:3000/${meal.image}`}
+                alt={meal.image}
+              />
+              <h3> {meal.name}</h3>
+              <p> ${meal.price}</p>
+              <p id="meal-item-description">{meal.description}</p>
+              <button className={"button"}>Add</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
