89da7a9e27ff75ac0aae072a38bd59a4a18b625f
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-03-19 22:43:34 +1000

    fix styling bugs.

--- src/components/Meals.tsx
+++ src/components/Meals.tsx
@@ -2,7 +2,6 @@
 export default function Meals({ meals, isLoading, loadingText, fallbackText }) {
   return (
     <>
-      {" "}
       {isLoading && (
         <div className={"mx-auto text-center border-b-orange-700"}>
           <p className={"text-red-600"}>{loadingText}</p>
@@ -18,13 +17,16 @@ export default function Meals({ meals, isLoading, loadingText, fallbackText }) {
                   src={`http://localhost:3000/${meal.image}`}
                   alt={meal.image}
                 />
-                <h3> {meal.name}</h3>
-                <p className={"meal-item-price"}>
-                  {" "}
-                  {currencyFormatter.format(meal.price)}
+                <div>
+                  <h3> {meal.name}</h3>
+                  <p className={"meal-item-price"}>
+                    {currencyFormatter.format(meal.price)}
+                  </p>
+                  <p id="meal-item-description">{meal.description}</p>
+                </div>
+                <p className={"meal-item-actions"}>
+                  <button className={"button"}>Add to Chart</button>
                 </p>
-                <p id="meal-item-description">{meal.description}</p>
-                <button className={"button"}>Add</button>
               </article>
             </li>
           ))}
