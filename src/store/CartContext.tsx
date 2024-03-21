e4c3db0f66e8d5e21ec083731180e3483422dc1e
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-03-21 23:56:57 +1000

    add ADD_ITEM action in reducer.

--- src/store/CartContext.tsx
+++ src/store/CartContext.tsx
@@ -8,24 +8,53 @@ export const CartContext = createContext({
 const CartReducer = (state, action) => {
   // ... update state to add meal item
   if (action.type === "ADD_ITEM") {
+    const updatedItems = [...state.items];
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id == action.payload,
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+    if (existingCartItem) {
+      updatedItems[existingCartItemIndex] = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+    } else {
+      updatedItems.push({
+        ...state.items,
+        quantity: 1,
+      });
+    }
+    return { ...state, items: updatedItems };
   }
 
   // ... update state to update meal item quantity
   if (action.type === "UPDATE_ITEM") {
+    const updatedItems = [...state.items];
+    const updatedItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id == action.payload.mealItemId,
+    );
+    //...TBC
   }
 
   return state;
 };
 function CartProvider({ children }) {
-  useReducer(CartReducer, { items: [] });
+  const [cartState, cartDispatch] = useReducer(CartReducer, { items: [] });
 
-  function addItemToCart() {}
-  function updateItemQuantity() {}
+  function handelAddItemToCart(id: string) {
+    cartDispatch({ type: "ADD_ITEM", payload: id });
+  }
+  function handelUpdateItemQuantity(id: string, amount: number) {
+    cartDispatch({
+      type: "UPDATE_ITEM",
+      payload: { mealItemId: id, amount: amount },
+    });
+  }
 
   const context = {
-    items: [],
-    addItemToCart: addItemToCart,
-    updateItemQuantity: updateItemQuantity,
+    items: cartState.items,
+    addItemToCart: handelAddItemToCart,
+    updateItemQuantity: handelUpdateItemQuantity,
   };
 
   return (
