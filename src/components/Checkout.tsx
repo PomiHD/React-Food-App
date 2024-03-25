ccccfc35faa17acb123e607e89a6a6db8ac8be78
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-03-25 23:07:50 +1000

    fix submit order bugs

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -6,6 +6,7 @@ import { UserProgressContext } from "../store/UserProgressContext.tsx";
 import { placeOrder } from "../htpp.ts";
 import { currencyFormatter } from "../util/formatting.ts";
 import Input from "../UI/Input.tsx";
+import { Error } from "./Error.tsx";
 
 export default function Checkout() {
   const { items } = useContext(CartContext);
@@ -15,17 +16,17 @@ export default function Checkout() {
     0,
   );
 
-  const nameRef = useRef();
-  const emailRef = useRef();
-  const addressRef = useRef();
-  const postcodeRef = useRef();
-  const cityRef = useRef();
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const streetRef = useRef("");
+  const postcodeRef = useRef("");
+  const cityRef = useRef("");
 
   const [emailIsInvalid, setEmailIsInvalid] = useState(false);
   const [errorUpdatingOrder, setErrorUpdatingOrder] = useState(false);
-  async function handelPlaceOrder(orderData) {
+  async function handelPlaceOrder(items, customerData) {
     try {
-      await placeOrder(orderData);
+      await placeOrder(items, customerData);
     } catch (error) {
       console.log(error.message);
       setErrorUpdatingOrder({
@@ -36,60 +37,24 @@ export default function Checkout() {
   }
   function handelSubmit(event) {
     event.preventDefault();
-    const enteredEmail = emailRef.current.value;
-    const emailIsValid = enteredEmail.includes("@");
-    const enteredName = nameRef.current.value;
-    const enteredAddress = addressRef.current.value;
-    const enteredPostcode = postcodeRef.current.value;
-    const enteredCity = cityRef.current.value;
-    const orderData = {
-      customer: {
-        name: enteredName,
-        email: enteredEmail,
-        address: enteredAddress,
-        postcode: enteredPostcode,
-        city: enteredCity,
-      },
-      items: items.map((item) => ({
-        id: item.id,
-        price: currencyFormatter.format(item.price),
-        quantity: item.quantity,
-      })),
-    };
-    const order = {
-      ...orderData,
-      totalPrice: currencyFormatter.format(totalPrice),
-    };
-
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+    // validate email address
+    const emailIsValid = customerData.email.includes("@");
     if (!emailIsValid) {
       setEmailIsInvalid(true);
       setErrorUpdatingOrder({
-        message: "Please enter a valid email",
+        message: "Please enter a valid email address.",
       });
-      console.log("stop sending http request...");
-      // return to stop the function execution
-      return;
+      return (
+        <Error
+          message={"Please enter a valid email address."}
+          title={"Invalid Email Address"}
+        />
+      );
     }
 
-    if (
-      !enteredEmail ||
-      !enteredName ||
-      !enteredAddress ||
-      !enteredPostcode ||
-      !enteredCity
-    ) {
-      setErrorUpdatingOrder({
-        message:
-          "Missing data: Email, name, street, postal code or city is missing.",
-      });
-      console.log(
-        "Missing data: Email, name, street, postal code or city is missing.",
-      );
-      return;
-    }
-    setEmailIsInvalid(false);
-    handelPlaceOrder(order);
-    console.log("sending http request...");
+    handelPlaceOrder(items, customerData);
   }
 
   return (
@@ -102,21 +67,29 @@ export default function Checkout() {
         <p>
           Total Amount: <strong>{currencyFormatter.format(totalPrice)}</strong>
         </p>
-        <Input label={"Full Name"} type={"text"} id={"full-name"} />
-        <Input label={"Email Address"} type={"email"} id={"email"} />
-        <Input label={"Street"} type={"text"} id={"street"} />
+        <Input label={"Full Name"} type={"text"} id={"name"} ref={nameRef} />
+        <Input
+          label={"Email Address"}
+          type={"email"}
+          id={"email"}
+          ref={emailRef}
+        />
+        <Input label={"Street"} type={"text"} id={"street"} ref={streetRef} />
         <div className={"control-row"}>
-          <Input label={"Postal Code"} type={"text"} id={"postal-code"} />
-          <Input label={"City"} type={"text"} id={"city"} />
+          <Input
+            label={"Postal Code"}
+            type={"text"}
+            id={"postal-code"}
+            ref={postcodeRef}
+          />
+          <Input label={"City"} type={"text"} id={"city"} ref={cityRef} />
         </div>
 
         <p className={"modal-actions"}>
           <Button textOnly onClick={showCart}>
             Back
           </Button>
-          <Button type="submit" onClick={hideCheckout}>
-            Submit Order
-          </Button>
+          <Button type="submit">Submit Order</Button>
         </p>
       </form>
     </Modal>
--- src/htpp.ts
+++ src/htpp.ts
@@ -8,14 +8,19 @@
   return resData;
 }
 
-export async function placeOrder(order) {
+export async function placeOrder(items, customerData) {
   const response = await fetch(`http://localhost:3000/orders`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     // the body must follow the same structure as the server expects
-    body: JSON.stringify({ order: order }),
+    body: JSON.stringify({
+      order: {
+        items: items,
+        customer: customerData,
+      },
+    }),
   });
   const resData = await response.json();
   if (!response.ok) {
