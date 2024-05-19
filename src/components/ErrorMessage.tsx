027652f8c4bc25f806c183f60015031ed6cbe8dd
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-05-19 21:49:18 +1000

    better Error handling

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,12 @@
+﻿type ErrorProps = {
+  title: string;
+  message: string;
+};
+export function ErrorMessage({ title, message }: ErrorProps) {
+  return (
+    <aside id="error">
+      <h1>{title}</h1>
+      <p>{message}</p>
+    </aside>
+  );
+}
